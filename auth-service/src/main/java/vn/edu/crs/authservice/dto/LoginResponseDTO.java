671abec154308b3fdf5ce1be37e34b0d33d5bd16
hoangtunang <ngohaituong23@gmail.com>
2026-09-03 09:16:45 +0700

    feat(auth-service): add userId claim to JWT

--- auth-service/src/main/java/vn/edu/crs/authservice/dto/LoginResponseDTO.java
+++ auth-service/src/main/java/vn/edu/crs/authservice/dto/LoginResponseDTO.java
@@ -1,11 +1,10 @@
-// path: auth-service/src/main/java/vn/edu/crs/authservice/dto/LoginResponseDTO.java
-// purpose: DTO tra ve token va thong tin co ban sau khi dang nhap thanh cong
 package vn.edu.crs.authservice.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 @Data
 @AllArgsConstructor
 public class LoginResponseDTO {
+    private Long userId;
     private String token;
     private String username;
     private String role;
--- auth-service/src/main/java/vn/edu/crs/authservice/service/AuthService.java
+++ auth-service/src/main/java/vn/edu/crs/authservice/service/AuthService.java
@@ -21,9 +21,9 @@ public class AuthService {
         {
             throw new InvalidCredentialsException("Sai username hoac password");
         }
-        String token = jwtUtil.generateToken(user.getUsername(),
+        String token = jwtUtil.generateToken(user.getId(), user.getUsername(),
                 user.getRole());
-        return new LoginResponseDTO(token, user.getUsername(),
+        return new LoginResponseDTO(user.getId(), token, user.getUsername(),
                 user.getRole());
     }
 }
